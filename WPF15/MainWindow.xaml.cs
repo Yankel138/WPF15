68c70fcc676ecc7c928c04c3f0697865d1b3bac1
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2022-01-11 00:36:59 +0300

    .

--- WPF15/MainWindow.xaml.cs
+++ WPF15/MainWindow.xaml.cs
@@ -9,12 +9,16 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Xps;
 using System.Windows.Xps.Packaging;
+using System.Xaml;
+using XamlReader = System.Windows.Markup.XamlReader;
+using XamlWriter = System.Windows.Markup.XamlWriter;
 
 namespace WPF15
 {
@@ -28,12 +32,27 @@ namespace WPF15
             InitializeComponent();
         }
 
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
-        //    XpsDocument doc = new XpsDocument("1.xps", FileAccess.Write);
-        //    XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(doc);
-        //    writer.Write(documentViewer.Document as FixedDocument);
-        //    doc.Close();
-        //}
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+            {
+                docViewer.Document = XamlReader.Load(fs) as FlowDocument;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Create))
+            {
+                XamlWriter.Save(docViewer.Document, fs);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+
+       
     }
 }
